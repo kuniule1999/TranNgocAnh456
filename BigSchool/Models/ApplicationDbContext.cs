b159ad17cc3098f5d2e3ad4f1c3ebc7283a50d78
Administrator <Administrator@PC237>
2020-06-04 11:25:30 +0700

    gui bai lan 2

--- BigSchool/Models/ApplicationDbContext.cs
+++ BigSchool/Models/ApplicationDbContext.cs
@@ -11,8 +11,10 @@ namespace BigSchool.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Course> Courses { get; set; }
+
         public DbSet<Category> Categories { get; set; }
         public ApplicationDbContext()
+
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
