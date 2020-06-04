1f3f1d121bc5c4d605026e094f4e687e66118b2f
Administrator <Administrator@PC237>
2020-06-04 09:58:37 +0700

    fa

---
+++ BigSchool/Models/Coure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.Models
+{
+    public class Coure
+    {
+        public int Id { get; set; }
+        public ApplicationUser lecturer { get; set; }
+        [Required]
+        public string LecturerId
+    }
+}
